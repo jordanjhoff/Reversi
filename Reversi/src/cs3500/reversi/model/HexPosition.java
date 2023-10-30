84d43e3df0a8569020c3f259f4d74a91e7ef7841
kellanmccarthy <kellanpmccarthy@gmail.com>
2023-10-30 14:14:29 -0400

    hexposition javadoc

--- Reversi/src/cs3500/reversi/model/HexPosition.java
+++ Reversi/src/cs3500/reversi/model/HexPosition.java
@@ -2,11 +2,25 @@ package cs3500.reversi.model;
 
 import java.util.Objects;
 
+/**
+ * Class to represent a hexagonal position, represented by
+ * three coordinates q, r, and s.
+ *
+ * A position is invalid solely if s != -q - r.
+ */
 public class HexPosition {
   private final int q;
   private final int r;
   private final int s;
 
+  /**
+   * Constructs a new position due to the three inputs, where s must equal
+   * -q - r.
+   * @param q q coordinate
+   * @param r r coordinate
+   * @param s s coordinate
+   * @throws IllegalArgumentException if position is invalid/illegal
+   */
   public HexPosition(int q, int r, int s) {
     this.q = q;
     this.r = r;
@@ -17,6 +31,12 @@ public class HexPosition {
     }
   }
 
+  /**
+   * Copy constructor for generating a new object of a position
+   * with the same values as the parameter to avoid aliasing issues.
+   * @param pos to be copied.
+   * @throws IllegalArgumentException if the given position is invalid
+   */
   public HexPosition(HexPosition pos) {
     this.q = pos.q;
     this.r = pos.r;
@@ -27,14 +47,26 @@ public class HexPosition {
     }
   }
 
+  /**
+   * Returns value of q.
+   * @return this position's Q
+   */
   public int getQPosition() {
     return this.q;
   }
 
+  /**
+   * Returns value of r.
+   * @return this position's R
+   */
   public int getRPosition() {
     return this.r;
   }
 
+  /**
+   * Returns value of S.
+   * @return this position's S
+   */
   public int getSPosition() {
     return this.s;
   }
