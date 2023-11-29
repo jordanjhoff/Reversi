e37dd48cf3d24882ee8cbfaa12ebbc8bf5f91d9e
Jordy <thejayhoffman@gmail.com>
2023-11-28 19:34:51 -0500

    added controller and multiple gui support

--- Reversi/src/cs3500/reversi/strategy/VisualUserStrat.java
+++ Reversi/src/cs3500/reversi/strategy/VisualUserStrat.java
@@ -5,7 +5,6 @@ import cs3500.reversi.model.ReadonlyHexReversiModel;
 import cs3500.reversi.model.TeamColor;
 
 public class VisualUserStrat implements ReversiStrategy {
-
   @Override
   public HexPosition choosePosn(ReadonlyHexReversiModel model, TeamColor color) {
     return null;
