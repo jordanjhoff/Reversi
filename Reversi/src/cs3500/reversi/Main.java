62289acc343f8b0cf4b42f692a6415b72a6a87ef
kellanmccarthy <kellanpmccarthy@gmail.com>
2023-11-30 10:31:06 -0500

    main method updates

--- Reversi/src/cs3500/reversi/Main.java
+++ Reversi/src/cs3500/reversi/Main.java
@@ -6,6 +6,7 @@ import cs3500.reversi.controller.Player;
 import cs3500.reversi.controller.VisualController;
 import cs3500.reversi.model.HexReversi;
 import cs3500.reversi.model.ReadonlyHexReversiModel;
+import cs3500.reversi.model.ReadonlyReversiModel;
 import cs3500.reversi.model.ReversiModel;
 import cs3500.reversi.model.TeamColor;
 import cs3500.reversi.strategy.CaptureMost;
@@ -22,14 +23,28 @@ public class Main {
    * A main method to execute and view a HexReversi game.
    */
   public static void main(String[] args) {
+
+    if (args.length == 0) {
+      // default case to figure out
+    }
+    else if (args == null || args.length != 3) {
+      throw new IllegalArgumentException("Code 1");
+    }
+    try {
+      // FIRST NUMBER IS BOARD SIZE
+      ReversiModel model = new HexReversi(Integer.parseInt(args[0]));
+      Player player1 = parsePlayer(args[1], TeamColor.BLACK, new ReadonlyHexReversiModel(model));
+      Player player2 = parsePlayer(args[1], TeamColor.WHITE, new ReadonlyHexReversiModel(model));
+      IReversiView viewPlayer1 = new ReversiGUIView(model);
+      IReversiView viewPlayer2 = new ReversiGUIView(model);
+      VisualController controller1 = new VisualController(model, viewPlayer1, player1);
+      VisualController controller2 = new VisualController(model, viewPlayer2, player2);
+      model.startGame();
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("invalid input");
+    }
+
     /*
-    ReversiStrategy textStrat = new UserTextInput(new Scanner(System.in));
-    Appendable out = System.out;
-    Player white = new PlayerImpl(TeamColor.WHITE, new CaptureMost());
-    Player black = new PlayerImpl(TeamColor.BLACK, textStrat);
-    ReversiModel modelText = new HexReversi(2); */
-
-
     ReversiModel model = new HexReversi(3);
     IReversiView viewPlayer1 = new ReversiGUIView(model);
     IReversiView viewPlayer2 = new ReversiGUIView(model);
@@ -45,7 +60,19 @@ public class Main {
     VisualController controller2 = new VisualController(model, viewPlayer2, AIplayer2);
     model.startGame();
 
+     */
+  }
 
 
+  private static Player parsePlayer(String arg, TeamColor color, ReadonlyReversiModel rorModel) {
+    if (arg.toLowerCase().equals("player")) {
+      return new HumanPlayer(color);
+    }
+    else if (arg.toLowerCase().equals("ai")){
+      return  new AIPlayer(color, new CaptureMost(), rorModel);
+    }
+    else {
+      throw new IllegalArgumentException("illegal arguments");
+    }
   }
 }
