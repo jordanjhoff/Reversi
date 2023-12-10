39aabaa61c5017376dc919c5e615e593733626d1
kellanmccarthy <kellanpmccarthy@gmail.com>
2023-12-09 20:53:45 -0500

    main and strat tests [] square

--- Reversi/test/StrategyMockTests.java
+++ Reversi/test/StrategyMockTests.java
@@ -10,9 +10,11 @@ import cs3500.reversi.controller.VisualController;
 import cs3500.reversi.controller.VisualControllerMock;
 import cs3500.reversi.model.HexPosition;
 import cs3500.reversi.model.HexReversi;
+import cs3500.reversi.model.Position;
 import cs3500.reversi.model.ReversiMock;
 import cs3500.reversi.model.ReadonlyHexReversiModel;
 import cs3500.reversi.model.ReversiModel;
+import cs3500.reversi.model.SquareReversi;
 import cs3500.reversi.model.TeamColor;
 import cs3500.reversi.strategy.CaptureMost;
 import cs3500.reversi.strategy.ReversiStrategy;
@@ -41,12 +43,12 @@ public class StrategyMockTests {
   HexReversiController controller2;
   HexReversiController controllerMock2;
 
-
+  ReadonlyHexReversiModel ror;
 
   @Before
   public void initData() {
     mockOut = new StringBuilder();
-    mock = new ReversiMock(mockOut, new HexReversi(2));
+    mock = new ReversiMock(mockOut, new SquareReversi(4));
     captureMost = new CaptureMost();
     viewOut = new StringBuilder();
 
@@ -63,33 +65,33 @@ public class StrategyMockTests {
     controller2 = new VisualController(mock, view2mock, mock2);
     controllerMock2 = new VisualControllerMock(viewOut, controller2);
 
+    ror =  new ReadonlyHexReversiModel(mock);
+
     mock.startGame();
   }
 
   @Test
   public void testCaptureMostBreakingTie() {
     initData();
-    mock.addPiece(TeamColor.BLACK, captureMost.choosePosn(
-            new ReadonlyHexReversiModel(mock), TeamColor.BLACK));
+    Position pos = captureMost.choosePosn(ror, TeamColor.BLACK);
+    mock.addPiece(TeamColor.BLACK, pos);
     // Strategy checks all possible valid moves, in top down, left to right, in order
     // flip count is 1 for every single one here (first move),
     // so its breaking ties by picking top leftmost
     Assert.assertTrue(mockOut.toString().contains("getValidMoves\n" +
-            "flipCount: [1,-2,1], value: 1\n" +
-            "flipCount: [-1,-1,2], value: 1\n" +
-            "flipCount: [2,-1,-1], value: 1\n" +
-            "flipCount: [-2,1,1], value: 1\n" +
-            "flipCount: [1,1,-2], value: 1\n" +
-            "flipCount: [-1,2,-1], value: 1\n" +
-            "addPiece: [1,-2,1]"));
+            "flipCount: [0,1], value: 1\n" +
+            "flipCount: [1,0], value: 1\n" +
+            "flipCount: [2,3], value: 1\n" +
+            "flipCount: [3,2], value: 1\n" +
+            "addPiece: [0,1]"));
     mock.addPiece(TeamColor.WHITE, captureMost.choosePosn(
             new ReadonlyHexReversiModel(mock), TeamColor.WHITE));
 
     Assert.assertTrue(mockOut.toString().contains("getValidMoves\n" +
-            "flipCount: [2,-1,-1], value: 1\n" +
-            "flipCount: [-2,1,1], value: 1\n" +
-            "flipCount: [-1,2,-1], value: 1\n" +
-            "addPiece: [2,-1,-1]"));
+            "flipCount: [0,0], value: 1\n" +
+            "flipCount: [0,2], value: 1\n" +
+            "flipCount: [2,0], value: 1\n" +
+            "addPiece: [0,0]"));
     // another example
   }
 
@@ -102,46 +104,27 @@ public class StrategyMockTests {
             new ReadonlyHexReversiModel(mock), TeamColor.WHITE));
     mock.addPiece(TeamColor.BLACK, captureMost.choosePosn(
             new ReadonlyHexReversiModel(mock), TeamColor.BLACK));
+    Position pos = captureMost.choosePosn(ror, TeamColor.WHITE);
+    mock.addPiece(TeamColor.WHITE, pos);
+    pos = captureMost.choosePosn(ror, TeamColor.BLACK);
+    mock.addPiece(TeamColor.BLACK, pos);
+    pos = captureMost.choosePosn(ror, TeamColor.WHITE);
+    mock.addPiece(TeamColor.WHITE, pos);
 
     Assert.assertTrue(mockOut.toString().contains("getValidMoves\n" +
-            "flipCount: [-1,-1,2], value: 1\n" +
-            "flipCount: [-2,1,1], value: 1\n" +
-            "flipCount: [1,1,-2], value: 2\n" +
-            "addPiece: [1,1,-2]"));
+            "flipCount: [2,0], value: 3\n" +
+            "flipCount: [2,3], value: 1\n" +
+            "flipCount: [3,1], value: 2\n" +
+            "addPiece: [2,0]"));
+    pos = captureMost.choosePosn(ror, TeamColor.BLACK);
+    mock.addPiece(TeamColor.BLACK, pos);
     // out of these three, picks last because highest value, even though its bottom right
-
+    System.out.println(mockOut);
     mock.addPiece(TeamColor.WHITE, captureMost.choosePosn(
             new ReadonlyHexReversiModel(mock), TeamColor.WHITE));
     Assert.assertTrue(mockOut.toString().contains("getValidMoves\n" +
-            "flipCount: [-1,-1,2], value: 2\n" +
-            "flipCount: [-1,2,-1], value: 3\n" +
-            "addPiece: [-1,2,-1]"));
-    // again
-
-    mock.addPiece(TeamColor.BLACK, captureMost.choosePosn(
-            new ReadonlyHexReversiModel(mock), TeamColor.BLACK));
-    Assert.assertTrue(mockOut.toString().contains("getValidMoves\n" +
-            "flipCount: [-2,1,1], value: 3\n" +
-            "addPiece: [-2,1,1]"));
-    // one valid move picks it
-
-    System.out.println(mockOut);
-  }
-
-  @Test
-  public void testCaptureMostNoValidMovesReturnsNull() {
-    mock.addPiece(TeamColor.BLACK, new HexPosition(-1, 2));
-    mock.addPiece(TeamColor.WHITE, new HexPosition(1, 1));
-    mock.addPiece(TeamColor.BLACK, new HexPosition(-2, 1));
-    mock.addPiece(TeamColor.WHITE, new HexPosition(-1, -1));
-    mock.addPiece(TeamColor.BLACK, new HexPosition(2, -1));
-    Assert.assertNull(captureMost.choosePosn(new ReadonlyHexReversiModel(mock), TeamColor.WHITE));
-    mock.pass();
-    mock.addPiece(TeamColor.BLACK, captureMost.choosePosn(
-            new ReadonlyHexReversiModel(mock), TeamColor.BLACK));
-
-    Assert.assertTrue(mockOut.toString().contains("getValidMoves\n" +
-            "flipCount: [1,-2,1], value: 2\n" +
-            "addPiece: [1,-2,1]"));
+            "flipCount: [3,0], value: 1\n" +
+            "flipCount: [3,2], value: 1\n" +
+            "addPiece: [3,0]"));
   }
 }
